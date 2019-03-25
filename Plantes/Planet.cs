898d085a3d941a3d0848af626da4d47a59cbdb9b
Peter <ripiter15@wp.pl>
2019-03-25 11:31:27 +0100

    Add project files.

---
+++ Plantes/Planet.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantesOpgH1
+{
+    class Planet
+    {
+        #region priviate variable
+        private string name;
+        private double mass;
+        private double diameter;
+        private double density;
+        private double gravity;
+        private double rotationPeriod;
+        private double lenghtOfDay;
+        private double distanceFromSun;
+        private double orbitalPeriod;
+        private double orbitalVelocity;
+        private double meanTemperature;
+        private double numberOfMoons;
+        private bool ringSystem;
+        #endregion
+
+        static List<Planet> planetList = new List<Planet>();
+
+        #region Get set private
+        public  List<Planet> Plantes
+        {
+            get
+            {
+                return planetList;
+            }
+            set
+            {
+                planetList = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        public double Mass
+        {
+            get
+            {
+                return this.mass;
+            }
+            set
+            {
+                this.mass = value;
+            }
+        }
+        public double Diameter
+        {
+            get
+            {
+                return this.diameter;
+            }
+            set
+            {
+                this.diameter = value;
+            }
+        }
+        public double Density
+        {
+            get
+            {
+                return this.density;
+            }
+            set
+            {
+                this.density = value;
+            }
+        }
+        public double Gravity
+        {
+            get
+            {
+                return this.gravity;
+            }
+            set
+            {
+                this.gravity = value;
+            }
+        }
+        public double RotationPeriod
+        {
+            get
+            {
+                return this.rotationPeriod;
+            }
+            set
+            {
+                this.rotationPeriod = value;
+            }
+        }
+        public double LenghtOfDay
+        {
+            get
+            {
+                return this.lenghtOfDay;
+            }
+            set
+            {
+                this.lenghtOfDay = value;
+            }
+        }
+        public double DistanceFromSun
+        {
+            get
+            {
+                return this.distanceFromSun;
+            }
+            set
+            {
+                this.distanceFromSun = value;
+            }
+        }
+        public double OrbitalPeriod
+        {
+            get
+            {
+                return this.orbitalPeriod;
+            }
+            set
+            {
+                this.orbitalPeriod = value;
+            }
+        }
+        public double OrbitalVelocity
+        {
+            get
+            {
+                return this.orbitalPeriod;
+            }
+            set
+            {
+                this.orbitalPeriod = value;
+            }
+        }
+        public double MeanTemperature
+        {
+            get
+            {
+                return this.meanTemperature;
+            }
+            set
+            {
+                this.meanTemperature = value;
+            }
+        }
+        public double NumberOfMoons
+        {
+            get
+            {
+                return this.numberOfMoons;
+            }
+            set
+            {
+                this.numberOfMoons = value;
+            }
+        }
+        public bool RingSystem
+        {
+            get
+            {
+                return this.ringSystem;
+            }
+            set
+            {
+                this.ringSystem = value;
+            }
+        }
+        #endregion
+
+        public Planet() { }
+        public Planet(string name, double mass, double diameter, double density, double gravity,
+                      double rotationPeriod, double lenghtOfDay, double distanceFromSun, double orbitalPeriod,
+                      double  orbitalVelocity, double meanTemperature, double numberOfMoons, bool ringSystem)
+        {
+            //This. points at the --> Planet(...) 
+            this.name = name;
+            this.mass = mass;
+            this.diameter = diameter;
+            this.density = density;
+            this.gravity = gravity;
+            this.rotationPeriod = rotationPeriod;
+            this.lenghtOfDay = lenghtOfDay;
+            this.distanceFromSun = distanceFromSun;
+            this.orbitalPeriod = orbitalPeriod;
+            this.orbitalVelocity = orbitalVelocity;
+            this.meanTemperature = meanTemperature;
+            this.numberOfMoons = numberOfMoons;
+            this.ringSystem = ringSystem;
+
+            planetList.Add(this);
+
+        }
+
+    }
+}
---
+++ Plantes/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantesOpgH1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Planet logic = new Planet();
+
+            Planet Mercury = new Planet("Mercury", 0.330, 4879, 5427, 3.7, 1407.6, 4222.6, 57.9, 88.0, 47.4, 167, 0, false);
+            Planet Earth = new Planet("Earth", 5.97, 12756, 5514, 9.8, 23.9, 24.0, 149.6, 365.2, 29.8, 15, 1, false);
+            Planet Mars = new Planet("Mars", 0.642, 6792, 3933, 3.7, 24.6, 24.7, 227.9, 687.0, 24.1, -65, 2, false);
+            Planet Jupiter = new Planet("Jupiter", 1898, 142984, 1326, 23.1, 9.9, 9.9, 778.6, 4331, 13.1, -110, 67, true);
+            Planet Saturn = new Planet("Saturn", 568, 120536, 687, 9.0, 10.7, 10.7, 1433.5, 10747, 9.7, -140, 62, true);
+            Planet Uranus = new Planet("Uranus", 86.8, 51118, 1271, 8.7, -17.2, 17.2, 2872.5, 30589, 6.8, -195, 27, true);
+            Planet Neptune = new Planet("Neptune", 102, 49528, 1638, 11.0, 16.1, 16.1, 4495.1, 59800, 5.4, -200, 14, true);
+            Planet Pluto = new Planet("Pluto", 0.0146, 2370, 2095, 0.7, -153.3, 153.3, 5906.4, 90560, 4.7, -225, 5, false);
+            Planet Venus = new Planet("Venus", 4.87, 12104, 5243, 8.9, -5832.5, 2802.0, 108.2, 224.7, 35.0, 464, 0, false);
+
+            PrintList(logic.Plantes);
+
+            Console.WriteLine(logic.Name);
+
+            //Removes Steve
+            //Bye Bye Steve
+            //logic.Plantes.Remove(steve);
+            //R.I.P. steve
+
+
+
+
+            //foreach (Planet e in logic.Plantes) {
+            //    Console.WriteLine(e.Name);
+            //    Console.WriteLine();
+            //}
+
+            //checks if the temperatur is below 0
+            //and if it is below 0 add to meanplanet
+            Console.WriteLine("Mean planned");
+            List<Planet> MeanPlanet = new List<Planet>();
+            foreach(Planet eee in logic.Plantes)
+            {
+              //  PrintList(logic.Plantes);
+                if (eee.MeanTemperature <= 0)
+                {
+                    MeanPlanet.Add(eee);
+                }
+            }
+            PrintList(MeanPlanet);
+            
+
+
+
+            Console.ReadLine();
+        }
+        //If typed planetlist();
+        //its like console.writeline but better
+        public static void PrintList(List<Planet> planetList)
+        {
+            foreach (Planet planet in planetList)
+            {
+                Console.WriteLine(planet.Name);
+            }
+            Console.WriteLine("\n");
+
+        }
+    }
+}
